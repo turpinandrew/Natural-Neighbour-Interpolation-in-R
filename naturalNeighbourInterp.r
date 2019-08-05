72ac6c6afe60dd03bdaafe4892f6286ee13288f5
Andrew Turpin <aturpin@unimelb.edu.au>
2019-08-06 09:28:25 +1000

    Allowed for matrix input and also fixed up case when (x,y) outside range(xs,ys)

--- naturalNeighbourInterp.r
+++ naturalNeighbourInterp.r
@@ -11,6 +11,7 @@
 # Modified 7 Jul 2014: added rw to first deldir in case x or y is outside xs or ys
 # Modified 13 Jun 2018: altered algorithm to be correct!
 # Modified 31 Jul 2019: complete rewrite to use areas, not segments
+# Modified  6 Aug 2019: allow for points well outside of the area of xs and ys, and matrix argument
 #
 
 require(deldir)
@@ -30,13 +31,16 @@ getNNWeights <- function(xs, ys, x, y, plotIt=FALSE) {
         return(list(neighbours=NA, weights=NA))
     }
 
-    d1<-deldir(xs,ys,plotit=plotIt, suppressMsge=TRUE)# Get Voronoi tiles without (x,y) 
-    if (x < d1$rw[1] || x > d1$rw[2])
-        warning(sprintf("getNNWeights: x (%f) is outside the rectangle bounding xs",x))
-    if (y < d1$rw[3] || y > d1$rw[4])
-        warning(sprintf("getNNWeights: y (%f) is outside the rectangle bounding ys",y))
+    rw <- c(range(xs), range(ys))
+    if (x < rw[1] || x > rw[2])
+        warning(sprintf("getNNWeights: x (%f) is outside the rectangle bounding xs.  Extending rectangle to include.",x))
+    if (y < rw[3] || y > rw[4])
+        warning(sprintf("getNNWeights: y (%f) is outside the rectangle bounding ys.  Extending rectangle to include.",y))
 
-    d2<-deldir(c(xs,x),c(ys,y),plotit=FALSE, suppressMsge=TRUE) # Get tiles with (x,y)
+    rw <- c(range(xs, x), range(ys,y))
+    d1<-deldir(xs,ys,rw=rw,plotit=plotIt, asp=1, suppressMsge=TRUE)# Get Voronoi tiles without (x,y) 
+
+    d2<-deldir(c(xs,x),c(ys,y),rw=rw,plotit=FALSE, suppressMsge=TRUE) # Get tiles with (x,y)
 
         # Work out the changed tiles between d1 and d2 (ignoring new one),
         # compute the differences in their area, and then normalise.
@@ -48,7 +52,7 @@ getNNWeights <- function(xs, ys, x, y, plotIt=FALSE) {
         n <- nrow(d1$summary)
         newSegs <- rbind(d2$dirsgs[d2$dirsgs$ind1 == n+1, 1:4],
                      d2$dirsgs[d2$dirsgs$ind2 == n+1, 1:4])
-        text(xs, ys, paste("(",1:n, ") ",zs,"dB",sep=""))
+        #text(xs, ys, paste("(",1:n, ") ",zs,"dB",sep=""))
         points(x,y,col="red",pch=19)
         
         segments(newSegs[,1], newSegs[,2], newSegs[,3], newSegs[,4], col=rgb(1,0,0,0.2))
@@ -60,7 +64,24 @@ getNNWeights <- function(xs, ys, x, y, plotIt=FALSE) {
 ##############################################################
 # Return value for (x,y) interp'ing on (xs,ys,zs)
 ##############################################################
-nnInterp <- function(xs, ys, zs, x, y, plotIt=FALSE) {
+nnInterp <- function(xs, ys=NULL, zs=NULL, x, y=NULL, plotIt=FALSE) {
+
+    if (is.matrix(xs) || is.data.frame(xs)) {
+        ys <- xs[,2]
+        zs <- xs[,3]
+        xs <- xs[,1]
+    }
+
+    if (length(x) == 2) {
+        y <- x[2]
+        x <- x[1]
+    }
+
+    if (is.null(ys) || is.null(zs))
+        stop("nnInterp cannot proceed: xs should be a 3 column matrix, or specify xs,ys,zs separately.")
+    if (is.null(y))
+        stop("nnInterp cannot proceed: x can be a length 2 vector of (x,y), or specify x and y separately.")
+
         # check if (x,y) is not already in zs
     ixy <- intersect(which(xs == x), which(ys == y))
     if (length(ixy) != 0) {
@@ -83,6 +104,7 @@ nnInterp <- function(xs, ys, zs, x, y, plotIt=FALSE) {
 #########################################
 ###test <- function(xs, ys, zs, x, y, answer) {
 ###    #print(getNNWeights(xs,ys,x,y,plotIt=TRUE))
+###    #print(nnInterp(xs,ys,zs,x,y,plotIt=TRUE))
 ###    print(abs(nnInterp(xs,ys,zs,x,y,plotIt=TRUE) - answer) < 1e-05)
 ###}
 ###
@@ -96,15 +118,18 @@ nnInterp <- function(xs, ys, zs, x, y, plotIt=FALSE) {
 ###zs <- 1:4
 ###test(xs,ys,zs,  0,  0, sum(0.25*zs))
 ###
-###test(xs,ys,zs,0.5,0.5, 2.867021)
+###test(xs,ys,zs,0.5,0.5, 2.85)
 ###
-####abline(h=c(-1.2, 1.2, -0.5, 0.3), v=c(-0.5, 0.3, 1.2, -1.2), col="green")
-#### Pt 1 - tri with area 0.5 * 0.5^2 = 1/8
-#### Pt 2 - trap area 0.5 * (0.5 + 0.1) * 1.2 = 0.36
-#### Pt 3 - rec + rec + tri = 0.3*1.2 + (1.2-0.3)*0.3 + 0.5*(1.2-0.3)^2 = 1.035
-#### Pt 4 - trap 0.5 * (0.5 + 0.1) * 1 = 0.36
-#### sum(c(1/8,0.36,1.035,0.36)/sum(c(1/8, 0.36,1.035,0.36)) * 1:4) = 2.867021
+###abline(h=c(-1.2, 0.5, -0.17, -0.5), v=c(-0.5, -0.17,0.5, -1.2), col="green")
+#### Pt 1 - tri with area 0.5 * 0.5^2 = 1/8 = 0.125
+#### Pt 2 - trap area 0.5 * (0.5 + 0.15) * 1 = 0.325
+#### Pt 3 - rec - tri = 1 - 0.5^3 = 0.875
+#### Pt 4 - Pt 2 = 0.325
+#### sum(c(1/8,0.325,0.875,0.325)/sum(c(1/8,0.325,0.875,0.325)) * 1:4) = 2.85
 ###
-###test(xs,ys,zs,20,20, 0)  # should warn
+###test(xs,ys,zs,20,20, 3)  # should warn
 ###
 ###test(c(-15,-9,-9),c(-9,-9,-15),c(30,30,30),-13,-15, 30)
+###
+###test(c(-21,21,-15,15), c(3,3,21,21), c(10,15,20,25), 1,1, 16.95234)
+###test(c(-21,21,-15,15), c(3,3,21,21), c(10,15,20,25), -1,-1, 15.59268)
